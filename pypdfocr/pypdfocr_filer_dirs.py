6ea69a371f8b007117cb90951027b206bfa31c41
obuenfil <ozbuenfil@gmail.com>
2016-03-20 16:30:53 -0500

    Update pypdfocr_filer_dirs.py

--- pypdfocr/pypdfocr_filer_dirs.py
+++ pypdfocr/pypdfocr_filer_dirs.py
@@ -28,6 +28,8 @@ class PyFilerDirs(PyFiler):
         self.target_folder = None
         self.default_folder = None
         self.original_move_folder = None
+        self.root_folder = None
+        self.inbox_folder = None
         self.folder_targets = {}
 
     def add_folder_target(self, folder, keywords):
@@ -55,12 +57,12 @@ class PyFilerDirs(PyFiler):
         if not foldername:
             logging.info("[DEFAULT] %s --> %s" % (newfilename, self.default_folder))
             
-            tgt_path = os.path.join(self.target_folder, self.default_folder.split('//',2)[0] +"/"+subfoldername +"/"+self.default_folder.split('//',2)[1])
+            tgt_path = os.path.join(os.path.join(self.root_folder, subfoldername), self.default_folder.split('/',1)[1])
             #tgt_path = os.path.join(self.target_folder, self.default_folder)
         else:   
             logging.info("[MATCH] %s --> %s" % (newfilename, foldername))
             
-            tgt_path = os.path.join(self.target_folder,foldername.split('//',2)[0] +"/"+subfoldername +"/"+foldername.split('//',2)[1])
+            tgt_path = os.path.join(os.path.join(self.target_folder, subfoldername), foldername)
             #tgt_path = os.path.join(self.target_folder,foldername)
 
         if not os.path.exists(tgt_path):
