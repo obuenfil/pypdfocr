be117462101cf8576d4cbeac615e1361e3863c61
obuenfil <ozbuenfil@gmail.com>
2016-03-21 19:41:20 -0500

    Update pypdfocr_filer_dirs.py

--- pypdfocr/pypdfocr_filer_dirs.py
+++ pypdfocr/pypdfocr_filer_dirs.py
@@ -59,12 +59,12 @@ class PyFilerDirs(PyFiler):
         if not foldername:
             logging.info("[DEFAULT] %s --> %s" % (newfilename, self.default_folder))
             
-            tgt_path = os.path.join(os.path.join(os.path.join(self.root_folder, subfoldername), self.target_folder.split('/',1)[1]), self.default_folder.rsplit('/',1)[0])
+            tgt_path = os.path.join(os.path.join(os.path.join("/"+self.root_folder, subfoldername), self.target_folder.split('/',1)[1]), self.default_folder.rsplit('/',1)[0])
             #tgt_path = os.path.join(self.target_folder, self.default_folder)
         else:   
             logging.info("[MATCH] %s --> %s" % (newfilename, foldername))
             logging.info(os.path.join("/"+ self.root_folder, subfoldername))
-            logging.info(os.path.join("/"os.path.join(self.root_folder, subfoldername),self.target_folder.split('/',1)[1]))
+            logging.info(os.path.join(os.path.join("/" + self.root_folder, subfoldername),self.target_folder.split('/',1)[1]))
             
             tgt_path = os.path.join(os.path.join(os.path.join("/"+self.root_folder, subfoldername),self.target_folder.split('/',1)[1]), foldername)
             #tgt_path = os.path.join(self.target_folder,foldername)
