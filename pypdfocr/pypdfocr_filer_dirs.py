8e0a6790de5880454b8ff1f7f5a2535bb0899479
obuenfil <ozbuenfil@gmail.com>
2016-03-21 21:55:26 -0500

    Update pypdfocr_filer_dirs.py

--- pypdfocr/pypdfocr_filer_dirs.py
+++ pypdfocr/pypdfocr_filer_dirs.py
@@ -66,9 +66,9 @@ class PyFilerDirs(PyFiler):
             logging.info(self.root_folder)
             logging.info(self.target_folder.split('/',1)[1])
             logging.info(os.path.join("/", subfoldername))
-            logging.info(os.path.join(os.path.join("/",  subfoldername),self.target_folder.split('/',1)[1]))
+            logging.info(os.path.abspath(os.path.join("/",  subfoldername),os.path.split(self.target_folder)[1]))
             
-            tgt_path = os.path.join(os.path.join(os.path.join("/", subfoldername),self.target_folder.split('/',1)[1]), foldername)
+            tgt_path = os.path.abspath(os.path.join(os.path.join("/", subfoldername),os.path.split(self.target_folder)[1]), foldername)
             #tgt_path = os.path.join(self.target_folder,foldername)
 
         if not os.path.exists(tgt_path):
